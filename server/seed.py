cfc2d34bad08c651860b9b40294e4b7e94b5c164
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-06-20 20:14:54 +0300

    Lab Done

--- server/seed.py
+++ server/seed.py
@@ -13,8 +13,34 @@ with app.app_context():
     pets.append(Pet(name = "Fido", species = "Dog"))
     pets.append(Pet(name = "Whiskers", species = "Cat"))
     pets.append(Pet(name = "Hermie", species = "Hamster"))
+    pets.append(Pet(name = "Slither", species = "Snake"))
+
 
     # Insert each Pet in the list into the database table
+    db.session.add_all(pets)
+
+    # Commit the transaction
+    db.session.commit()
+
+with app.app_context():
+
+    # Create and initialize a faker generator
+    fake = Faker()
+
+    # Delete all rows in the "pets" table
+    Pet.query.delete()
+
+    # Create an empty list
+    pets = []
+
+    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
+
+    # Add some Pet instances to the list
+    for n in range(10):
+        pet = Pet(name=fake.first_name(), species=rc(species))
+        pets.append(pet)
+
+    # Insert each Pet in the list into the "pets" table
     db.session.add_all(pets)
 
     # Commit the transaction
